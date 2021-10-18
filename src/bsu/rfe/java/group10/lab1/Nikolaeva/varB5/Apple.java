bd6c338d1c8ab3f0d237fa317e07f802598acb02
Dark_Soul <irina2002.nikolaeva@yandex.ru>
2021-10-18 22:27:24 +0300

    Added class Apple

---
+++ src/bsu/rfe/java/group10/lab1/Nikolaeva/varB5/Apple.java
@@ -0,0 +1,39 @@
+package bsu.rfe.java.group10.lab1.Nikolaeva.varB5;
+
+public class Apple extends Food {
+
+public String size;
+	
+	public Apple (String size) {
+		super("Apple");
+		this.size=size;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	@Override
+	public void consume() {
+		System.out.println(this+" was eaten ");
+	}
+
+	@Override
+	public boolean equals(Object arg0) {
+		if(!super.equals(arg0)) return false;
+		if (!(arg0 instanceof Apple)) return false;
+		return size.equals(((Apple)arg0).size);
+	}
+
+	@Override
+	public String toString() {
+		return "An "+super.toString()+" of '"+size.toUpperCase()+"' size ";
+	}
+
+	}
+
+
--- src/bsu/rfe/java/group10/lab1/Nikolaeva/varB5/Food.java
+++ src/bsu/rfe/java/group10/lab1/Nikolaeva/varB5/Food.java
@@ -28,4 +28,4 @@ public abstract class Food implements Consumable {
 
 	}
 
-}
+
